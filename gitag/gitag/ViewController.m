7d0da7245e7dc38990ab6cf058814efcaafe6c53
maoziyue <>
2018-03-02 20:49:53 +0800

    修改一

--- gitag/gitag/ViewController.m
+++ gitag/gitag/ViewController.m
@@ -16,14 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    //1.修改一
+    
+    
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
