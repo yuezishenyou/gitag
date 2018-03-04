23515b18ae3526a11a234828d8200bd9d799e6f5
maoziyue <>
2018-03-04 10:53:21 +0800

    abc

--- gitag/gitag/AppDelegate.m
+++ gitag/gitag/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //小顾修改一
+    
     return YES;
 }
 
